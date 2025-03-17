8963868fdaa7fcd23a07c0cb13f2fc1edac0a822
Aman Attri <aman.attri@assurant.com>
2025-03-17 15:56:03 +0530

    Initial Commit

---
+++ FirebaseSwiftUIExample/FirebaseSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSwiftUIExampleApp.swift
+//  FirebaseSwiftUIExample
+//
+//  Created by Aman Attri on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirebaseSwiftUIExampleTests/FirebaseSwiftUIExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSwiftUIExampleTests.swift
+//  FirebaseSwiftUIExampleTests
+//
+//  Created by Aman Attri on 17/03/25.
+//
+
+import Testing
+@testable import FirebaseSwiftUIExample
+
+struct FirebaseSwiftUIExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
